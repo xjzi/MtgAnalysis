6a55b431018d8e25b1e62424ce27c9259dd35218
Poultryghast <66976878+Poultryghast@users.noreply.github.com>
2021-05-11 17:25:45 -0400

    Broke up shared models

---
+++ Shared/Decks.cs
@@ -0,0 +1,17 @@
+﻿using Shared;
+
+public class Deck : SqlReferencedItem
+{
+	public string[] Mainboard { get; set; }
+	public string[] Sideboard { get; set; }
+}
+
+public class DeckWithCluster : Deck
+{
+	public Cluster Parent { get; set; }
+}
+
+public class DeckWithRank : Deck
+{
+	public int Rank { get; set; }
+}
---
+++ Shared/Other.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shared
+{
+	public abstract class SqlReferencedItem
+	{
+		public int Id { get; set; }
+	}
+
+	public class Game
+	{
+		public Deck Winner { get; set; }
+		public Deck Loser { get; set; }
+	}
+
+	public class HighlightCard
+	{
+		public Card Card { get; set; }
+		public Cluster Cluster { get; set; }
+		public double Frequency { get; set; }
+	}
+
+
+	public class Tournament
+	{
+		public string Url { get; set; }
+		public string CardSet { get; set; }
+		public string GameType { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
