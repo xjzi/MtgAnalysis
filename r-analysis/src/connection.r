2fb9812c467bd42745645e431156a7b2010b3d3a
Poultryghast <66976878+Poultryghast@users.noreply.github.com>
2021-06-18 14:18:42 -0400

    Update connection.r
    Accidentally contained the server IP, which should not be public yet. A new static IP was assigned to the server.

--- r-analysis/src/connection.r
+++ r-analysis/src/connection.r
@@ -4,7 +4,7 @@ connect <- function()
 {
 	con <- dbConnect(RPostgres::Postgres(),
 	dbname = "tournaments",
-	host = "54.88.146.98",
+	host = "db",
 	port = 5432,
 	user = "analysis",
 	password = Sys.getenv(x="PASSWORD"))
