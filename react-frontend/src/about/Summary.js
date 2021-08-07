6c6e54ba624889d6cdec7fa9bab0be2af18fff8c
Poultryghast <murp8752@gmail.com>
2021-08-07 08:00:52 -0400

    Adjusted styles and description for frontend

--- react-frontend/src/about/Summary.js
+++ react-frontend/src/about/Summary.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const summary = <React.Fragment>
     <p>In Magic: the Gathering, designing a powerful deck is often more complex than the main game. In order to make it easier to build competitive decks, this project synthesizes decks from tournaments and shows the results on this website. </p>
-    <p>Tournament decks are used to identify common themes with which players can build their decks. Advanced players can design their decks to counter these themes. On this website, each theme is shown as some of the central cards that support it. The page dedicated to that theme shows a deck that's designed around it.</p>
-    <p>For a more detailed technical explanation visit the <a href="https://github.com/Poultryghast/MtgAnalysis">Github Page</a>. Continue browsing by selecting a set of cards from the top menu.</p>
+    <p>Tournament decks are used to identify common themes with which players can build their decks. Advanced players can design their decks to counter these themes. Each theme is shown as some of the central cards that support it and the art image of one of these cards.</p>
+    <p>Clicking one of these images goes to a deck that's designed around the corresponding theme. Clicking any card will go to a third-party website that provides card details.</p>
+    <p>For a more detailed explanation of this project visit the <a href="https://github.com/Poultryghast/MtgAnalysis">Github Page</a>. Continue browsing by selecting a set of cards from the top menu.</p>
 </React.Fragment>
 export default summary;
